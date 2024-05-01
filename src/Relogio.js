92a14de73b503e2eb3fcc47c1685093b7cd14524
Rayan Anzai <100059542+Rayanzai@users.noreply.github.com>
2024-05-01 18:56:26 -0300

    Commit do relogio Funcionando

--- src/Relogio.js
+++ src/Relogio.js
@@ -1,11 +1,25 @@
+import React from "react"
 
+class Relogio extends React.Component {
 
-function Relogio() {
-  return (
-    <>
-    <h1>Meu Relógio</h1>
-    </>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTime: '00:00:00'
+    }
+  }
+
+  render(){
+    setInterval(() => {
+      this.setState({ currentTime: new Date().toLocaleTimeString() })
+    }, 1000 );
+    
+    return(
+      <>
+      <h1>Meu relógio</h1>
+      <h1>{this.state.currentTime}</h1>
+      </>
+    )
+  }
 }
-
 export default Relogio;
